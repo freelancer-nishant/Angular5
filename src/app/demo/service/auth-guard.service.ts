9b4c954ebdb0e439f03632be2fe10b5b3a9d6d7b
Jignasa <jignasa.gajera1984@gmail.com>
2018-03-15 18:51:08 +0530

    - Restrict state dropdown values to “California” - Restrict to access other pages from embeded page

--- src/app/demo/service/auth-guard.service.ts
+++ src/app/demo/service/auth-guard.service.ts
@@ -7,20 +7,26 @@ export class AuthGuardService implements CanActivate {
     constructor(public auth: AuthService, public router: Router) { }
 
     canActivate(route: ActivatedRouteSnapshot): boolean {
-        
+
         let expectedRole: string = route.data.expectedRole;
-        
+        let hasClientAccess: boolean = route.data.hasClientAccess;
+
         if (!this.auth.isAuthenticated()) {
             this.router.navigate(['/login']);
             return false;
         }
 
-        let tokenPayload: any = this.auth.decodeToken();        
+        let tokenPayload: any = this.auth.decodeToken();
         if (expectedRole != undefined) {
             if (tokenPayload.role !== expectedRole) {
                 this.router.navigate(['/login']);
                 return false;
             }
+        }
+
+        if (localStorage.getItem('isClientPage')=="true" && !hasClientAccess) {
+            this.router.navigate(['/login']);
+            return false;
         }
 
         return true;
